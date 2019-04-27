ac8b4c68b451ad626e3e30dfaf3edcdd9c898e1b
Vitaly <v@086.kz>
2019-04-27 19:50:59 +0600

    Add comments and readme

--- config.example.php
+++ config.example.php
@@ -1,18 +1,20 @@
 <?php
-    define('CONFIRMATION_TOKEN', '');
-    define('ACCESS_TOKEN', '');
-    define('SECRET_KEY', '');
-    define('VERSION', '');
-    define('GROUP_ID', '');
-    define('ADMIN_ID', '');
-    define('USER_ACCESS_TOKEN', '');
+    define('CONFIRMATION_TOKEN', ''); // Токен, который понадобится вывести, когда будешь подтверждать свой сервер, чтобы на сервер события приходили (если не хочешь, чтобы в телегу уведомления о новых подписчиках приходили, то не надо)
+    define('ACCESS_TOKEN', ''); // Токен ГРУППЫ
+    define('SECRET_KEY', ''); // Секретный ключ рядом с confirmation token (опять же, не надо, если уведомления в телегу не хочешь привязывать)
+    define('VERSION', ''); // Версия API VK
+    define('GROUP_ID', ''); // ID группы
+    define('ADMIN_ID', ''); // Твой ID
+    define('USER_ACCESS_TOKEN', ''); // Токен ЮЗЕРА (Необходимые права: offline, wall, photos)
 
-    define('ACCESS_KEY_TO_PANEL', '');
+    define('ACCESS_KEY_TO_PANEL', ''); // Пароль для доступа в админ-панель
 
+    // Инфа для доступа к базе
     define('DB_HOST', '');
     define('DB_NAME', '');
     define('DB_USERNAME', '');
     define('DB_PASSWORD', '');
 
+    // Телега тупо
     define('TELEGRAM_ACCESS_TOKEN', '');
     define('TELEGRAM_CHAT_ID', '');
